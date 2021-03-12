fa8d0d1a15e7f5f18186215a5b7d4461f328f1e3
Christopher Ross <christophe.rossie@gmail.com>
2021-03-12 11:43:38 -0800

    Update visibility and fxn naming

--- FinStats/Controllers/StocksController.cs
+++ FinStats/Controllers/StocksController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using FinStats;
-using FinStats.Models;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using FinStats.Data;
 
 namespace Controllers
 {
@@ -11,23 +9,22 @@ namespace Controllers
     [ApiController]
     public class StocksController : ControllerBase
     {
+        readonly AppDb Db;
         public StocksController(AppDb db)
         {
             Db = db;
         }
-
         // GET: api/<ValuesController>
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             await Db.Connection.OpenAsync();
-            var result = await StocksQuery.LatestStockAsync(Db);
+
+            var result = await StocksQuery.LatestAsync(Db);
 
             await Db.Connection.CloseAsync();
 
             return new OkObjectResult(result);
         }
-
-        public AppDb Db { get; }
     }
 }
