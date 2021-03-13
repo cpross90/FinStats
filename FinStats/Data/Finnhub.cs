e81623bddf24523af3a0ec5506c90f4ec508e1d7
Christopher Ross <christophe.rossie@gmail.com>
2021-03-12 16:48:52 -0800

    Testing different options

--- FinStats/Data/Finnhub.cs
+++ FinStats/Data/Finnhub.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.SignalR.Client;
 
 namespace FinStats.Data
@@ -6,12 +8,20 @@ namespace FinStats.Data
     public class Finnhub : IDisposable
     {
         public HubConnection Connection { get; }
+        private readonly string Token;
 
-        public Finnhub(string connectionString)
+        public Finnhub(string connectionString, string token)
         {
-            Connection = new HubConnectionBuilder().WithUrl(new Uri(connectionString))
+            Connection = new HubConnectionBuilder()
+                .WithUrl(new Uri(connectionString), options =>
+                {
+                    options.AccessTokenProvider = () => Task.FromResult(Token);
+                    options.Transports = HttpTransportType.WebSockets;
+                })
                 .WithAutomaticReconnect()
                 .Build();
+
+            Token = token;
         }
         public async void Dispose()
         {
